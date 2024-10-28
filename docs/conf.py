2a7d4b27382a17484d6ff5f76a6c22aaccc22fea
Carlson Büth <commit@cbueth.de>
2024-10-28 09:47:47 +0100

    Doc: change repo url to gitlab

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ numpydoc_xref_param_type = True
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_theme_options = {
-    "repository_url": "https://github.com/cbueth/infomeasure/",
+    "repository_url": "https://gitlab.ifisc.uib-csic.es/carlson/infomeasure",
     "repository_branch": "main",
     "use_source_button": True,
     "use_repository_button": True,
