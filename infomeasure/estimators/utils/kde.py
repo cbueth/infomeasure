5fbf0c87a1f9878c684809d16a33c69e5d58cda4
Carlson Büth <commit@cbueth.de>
2025-04-01 16:39:37 +0200

    Fix: Add gaussian kernel support for not full-rank data, as well as MI(x, x) (self-information)
    Signed-off-by: Carlson Büth <commit@cbueth.de>

--- infomeasure/estimators/utils/kde.py
+++ infomeasure/estimators/utils/kde.py
@@ -1,9 +1,10 @@
 """Kernel Density Estimation (KDE) utilities."""
 
-from numpy import abs as np_abs, newaxis, inf, number, issubdtype
+from numpy import abs as np_abs, ndarray, dot
 from numpy import all as np_all
-from numpy import asarray
+from numpy import argsort, asarray, cov, inf, issubdtype, newaxis, number
 from numpy import sum as np_sum
+from numpy.linalg import eig
 from scipy.spatial import KDTree
 from scipy.stats import gaussian_kde
 
@@ -49,8 +50,7 @@ def kde_probability_density_function(data, bandwidth, x=None, kernel="box"):
         x = x[newaxis, :]
 
     if kernel == "gaussian":
-        kde = gaussian_kde(data.T, bw_method=bandwidth)
-        return kde.evaluate(x.T).squeeze()
+        return gaussian_kernel_densities(data.T, x.T, bandwidth)
     elif kernel == "box" and x.shape[0] == 1:
         # Define the box kernel density estimation
         N, d = data.shape
@@ -78,3 +78,47 @@ def kde_probability_density_function(data, bandwidth, x=None, kernel="box"):
     # Another approach to box kernel density estimation
     else:  # TODO: Add more kernel types as needed
         raise ValueError(f"Unsupported kernel type: {kernel}. Use 'gaussian' or 'box'.")
+
+
+def gaussian_kernel_densities(data, x, bandwidth, eigen_threshold: float = 1e-10):
+    """Calculate kde for gaussian kernel.
+
+    In case of multivariate data, checks rank of data and reduces dimensions
+    if eigenvalues are below threshold.
+    If already full rank, does no reprojection.
+
+    Parameters
+    ----------
+    data : ndarray, shape (d, N)
+        Data points to estimate density for.
+    x : ndarray, shape (d, n)
+        Points at which to evaluate the density.
+    bandwidth : float
+        Bandwidth parameter for kernel density estimation.
+    eigen_threshold : float, optional
+        Threshold for eigenvalues to determine rank of data. Default is 1e-10.
+
+    Returns
+    -------
+    densities : ndarray, shape (n,)
+        Estimated density values at points x.
+    """
+    if data.shape[0] > 1:  # Multivariate case
+        # Calculate covariance matrix
+        covariance_matrix = cov(data)
+        # Get eigenvalues and eigenvectors
+        values, vectors = eig(covariance_matrix)
+        sorted_indices = argsort(values)[::-1]
+        values_sorted = values[sorted_indices]
+        vectors_sorted = vectors[:, sorted_indices]
+        # Get the number of eigenvalues greater than the threshold
+        num_non_zero_eigenvalues = np_sum(values_sorted > eigen_threshold)
+        # Check projection necessary
+        if num_non_zero_eigenvalues < data.shape[0]:
+            # Project the data onto the reduced space
+            pca_components = vectors_sorted[:, :num_non_zero_eigenvalues]
+            data = dot(data.T, pca_components).T
+            x = dot(x.T, pca_components).T
+
+    kde = gaussian_kde(data, bw_method=bandwidth)
+    return kde.evaluate(x).squeeze()
